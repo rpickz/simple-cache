d319bd0012b3d86361afb3bb3c83bd79e5b72079
Richard Pickering <richard.pickering@hotmail.co.uk>
2020-11-14 15:12:14 +0000

    Added wg/wrk based benchmark for HTTP handler func

---
+++ scripts/benchmarks/wrk_test_http.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# 1) Build the application.
+echo "Building the cache"
+cd ../../src/cmd/hashcacheweb
+go build -o simplecache .
+cd - >/dev/null
+mv ../../src/cmd/hashcacheweb/simplecache .
+
+# 2) Start the application.
+echo "Starting the cache"
+./simplecache &
+APP_PID=$!
+
+sleep 2
+
+# 3) Insert some test data.
+curl -X PUT -d'{"key":"value"}' http://localhost:8080/something
+
+if [ ! -d results ] ; then
+  mkdir results
+fi
+
+# 4) Run the tests and save results to file.
+echo "== Running GET benchmark =="
+wrk -t12 -c400 -d30s http://localhost:8080/something | tee results/get_results.txt
+
+echo "== Running PUT benchmark =="
+cat >wrk_put.lua <<-END
+wrk.method = "POST"
+wrk.body = "{\"key\":\"value\"}"
+END
+wrk -t12 -c400 -d30s -s wrk_put.lua http://localhost:8080/something | tee results/put_results.txt
+rm wrk_put.lua
+
+echo "== Running DELETE benchmark =="
+cat >wrk_del.lua <<-END
+wrk.method = "POST"
+wrk.body = "{\"key\":\"value\"}"
+END
+wrk -t12 -c400 -d30s -s wrk_del.lua http://localhost:8080/something | tee results/del_results.txt
+rm wrk_del.lua
+
+# 5) Kill the application.
+echo "Killing the cache application"
+kill $APP_PID
